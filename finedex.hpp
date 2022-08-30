b31fd92fb1501260a2a0bd6b89808274a06dc307
Dominik Horn <dominik.horn@tum.de>
2022-08-30 16:29:31 +0000

    implement FINEdex exposure
    NOTE: construction currently segfaults :)

--- finedex.hpp
+++ finedex.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#include "include/function.h"
+
+#include "include/aidel.h"
+#include "include/aidel_impl.h"
+
 namespace finedex {
-struct FINEdex {};
-}  // namespace finedex
+template <class key_t, class val_t>
+using FINEdex = aidel::AIDEL<key_t, val_t>;
+}
--- src/example.cpp
+++ src/example.cpp
@@ -1,6 +1,44 @@
 #include <finedex.hpp>
 
+#include <limits>
+#include <random>
+
 int main() {
-  finedex::FINEdex d;
+  using Key = uint64_t;
+  using Value = size_t;
+
+  const size_t n = 1000;
+  const size_t max_error = 42;
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<Value> val_dist(
+      std::numeric_limits<Value>::min(),
+      std::numeric_limits<Value>::max() >> 1ULL);
+
+  std::vector<Key> keys;
+  std::vector<Value> values;
+  keys.reserve(n);
+  values.reserve(n);
+
+  for (size_t i = 0; i < n; i++) {
+    keys.push_back(i * i);
+    values.push_back(val_dist(gen));
+  }
+
+  assert(keys.size() == n);
+  assert(values.size() == n);
+
+  // create and train findex
+  finedex::FINEdex<uint64_t, uint64_t> d;
+  d.train(keys, values, max_error);
+
+  // test that all values are correctly found
+  for (size_t i = 0; i < n; i++) {
+    Value test_val;
+    d.find(keys[i], test_val);
+    assert(test_val == values[i]);
+  }
+
   return 0;
 }
